c85233b14bbdc00f86f6f2cc1114183442099293
Alireza Khodayi <89078767+Alireza-Khodayi@users.noreply.github.com>
2023-05-18 07:34:52 +0330

    Starting Point

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Trivia Quiz App",
+  description: "Written with next.js and tailwind css",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col justify-center items-center my-10">
+      <h1 className="my-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Wellcome to Trivia Quiz App
+      </h1>
+    </section>
+  );
+}
